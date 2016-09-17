2c1528495e3dab336c1ede6337d817feaa4b0252
Rafael Eyng <rafaeleyng@gmail.com>
2016-09-17 02:51:08 -0300

    Updates

--- index.js
+++ index.js
@@ -88665,7 +88665,8 @@
 	        null,
 	        _react2.default.createElement(_Board2.default, { width: this.state.width, height: this.state.height, board: this.state.board, click: this.handleCellClick }),
 	        _react2.default.createElement(_Controls2.default, { isPlaying: this.state.gameState === 'playing', togglePlay: this.togglePlay, clear: this.clear, random: this.random }),
-	        camera
+	        camera,
+	        _react2.default.createElement('a-sky', { color: '#DDDDDD' })
 	      );
 	    }
 	  }]);
